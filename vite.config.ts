9c260af1865a2bdcc95b5dd5b540423c4ad45780
Mehrshad <code.rezaei@gmail.com>
2025-01-18 19:15:57 +0330

    This change might fix a problem:
    - 404 not found on sw.js and icons

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,14 @@ import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/PWA_NFC_Reader/',
   plugins: [react()],
   server: {
     https: {
-      // openssl req -x509 -newkey rsa:2048 -keyout certs/key.pem -out certs/cert.pem -days 365 -nodes -subj "/CN=localhost"
       key: fs.readFileSync('certs/key.pem'),
       cert: fs.readFileSync('certs/cert.pem'),
     },
     host: true,
     port: 5173,
   },
+  publicDir: 'public',
 })
